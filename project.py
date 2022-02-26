ea87307487d062a1a6c3e2d27d8bca8b295378ae
Wu Fatian <wuft91@gmail.com>
2022-02-26 15:23:57 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
-import joblib
 import pandas as pd
 import numpy as np
+from xgboost import XGBClassifier
 
-
-modelclf = joblib.load("model.m")
+modelclf = XGBClassifier()
+modelclf.load_model("./model/model.bin")
 
 
 def cal_pro(test_data):
@@ -13,6 +13,5 @@ def cal_pro(test_data):
     ndata = np.array(ldata).reshape((1, -1))
     dfdata = pd.DataFrame(ndata, columns=["Age", "Baseline heart rate", "Uric acid", "D-dimer", "Chlorine", "GCS", "GFR"])
     result = round(modelclf.predict_proba(dfdata)[0][1], 4)
-    print(result)
     return result
 
